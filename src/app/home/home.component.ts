f71e2321d4ceb16870ee110d60eb3b1b60b5bbd5
karu <laine.brice@gmail.com>
2017-08-23 14:48:36 -0400

    v1 Data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  image:string  = "assets/images/image1.jpg"
+  
+  constructor() {
+   }
 
   ngOnInit() {
   }
